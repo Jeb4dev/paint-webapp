b816b41ebd4ac84d53445f2d632c805b1b6d5d4e
jespe <jesperkauppinen@gmail.com>
2021-11-27 22:29:38 +0200

    + register form template

--- app.py
+++ app.py
@@ -1,23 +1,48 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
 from models import db
 from config import Config
-from login import login_manager
 
-from views.home import home
-from views.auth import auth
+login_manager = LoginManager()
 
-app = Flask(__name__)
 
-app.config.from_object(Config())
+def create_app():
+    _app = Flask(__name__)
 
-app.register_blueprint(home, url_prefix='/')
-app.register_blueprint(auth, url_prefix='/auth')
+    _app.config.from_object(Config())
 
-db.init_app(app)
-login_manager.init_app(app)
-migrate = Migrate(app, db)
+    # init plugins
+    db.init_app(_app)
+    login_manager.init_app(_app)
+
+    from views.home import home
+    from views.auth import auth
+
+    # register blueprints
+    _app.register_blueprint(home, url_prefix='/')
+    _app.register_blueprint(auth, url_prefix='/auth')
+
+    migrate = Migrate(_app, db)
+
+    from models.user import User
+
+    @login_manager.user_loader
+    def load_user(identifier):
+        return User.query.get(int(identifier))
+
+    # Error Management
+    @_app.errorhandler(404)
+    def page_not_found(error):
+        return redirect(url_for('views.home'))
+
+    db.create_all(app=_app)
+
+    return _app
+
+
+app = create_app()
+
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
--- views/auth.py
+++ views/auth.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, redirect, url_for
+from werkzeug.security import check_password_hash
 from forms.login import LoginForm
+from flask_login import login_user, login_required, logout_user, current_user
+from forms.register import RegisterForm
+from models.user import User
 
 auth = Blueprint('auth', __name__)
 
@@ -12,14 +15,38 @@ def account():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return "Already authenticated"
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        print("login", form.username.data, form.password.data)
-        return "Login redirect"
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(password, user.password_hash):
+                login_user(user, remember=True)
+                return redirect(url_for('auth.account'))
+            else: print("wrong pswd")
     return render_template('auth/login.html', form=form)
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-    return 'register page'
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return "username already exists"
+            # TODO: flash notification
+        else:
+            new_user = User.create(username, password)
+            print(f"{username} registered")
+            login_user(new_user, remember=True)
+            return redirect(url_for('auth.account'))
+
+    return render_template('auth/register.html', form=form)
