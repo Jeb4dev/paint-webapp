dd3a0d1a417ec6b6b2a28c7a0fa16e9b6560076c
jespe <jesperkauppinen@gmail.com>
2021-11-28 13:20:52 +0200

    flash notifications

--- views/auth.py
+++ views/auth.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
-from werkzeug.security import check_password_hash
+from flask import Blueprint, render_template, redirect, url_for, flash
 from forms.login import LoginForm
 from flask_login import login_user, login_required, logout_user, current_user
 from forms.register import RegisterForm
@@ -19,6 +18,7 @@ def account():
 @auth.route('/logout', methods=['GET'])
 def logout():
     logout_user()
+    flash("logged out successfully")
     return redirect(url_for('auth.account'))
 
 
@@ -37,13 +37,17 @@ def login():
             if user.check_password(password):
                 login_user(user, remember=True)
                 return redirect(url_for('auth.account'))
-            else: print("wrong pswd")
+            else:
+                flash("Invalid password")
+        else:
+            flash("Username does not exist")
     return render_template('auth/login.html', form=form)
 
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
+        flash("You are already logged in.")
         return redirect(url_for('auth.account'))
 
     form = RegisterForm()
@@ -53,12 +57,13 @@ def register():
         password = form.password.data
         user = User.query.filter_by(username=username).first()
         if user:
-            return "username already exists"
-            # TODO: flash notification
+            flash("username already exists")
+            return redirect(url_for('auth.register'))
         else:
             new_user = User.create(username, password)
             print(f"{username} registered")
             login_user(new_user, remember=True)
+            flash("Registering was successful")
             return redirect(url_for('auth.account'))
 
     return render_template('auth/register.html', form=form)
