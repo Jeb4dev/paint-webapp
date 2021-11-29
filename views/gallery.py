9f2d79980f1baeed2ccc36f78f962a44a9961715
jespe <jesperkauppinen@gmail.com>
2021-11-29 23:04:34 +0200

    return num

--- views/gallery.py
+++ views/gallery.py
@@ -6,4 +6,4 @@ gallery = Blueprint('gallery', __name__)
 
 @gallery.get('/')
 def index():
-    return render_template('gallery/index.html', user=current_user)
+    return render_template('gallery/index.html', user=current_user, images=6)
