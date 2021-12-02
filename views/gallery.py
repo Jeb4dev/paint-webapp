1ff8964eb21215e5602d91f0f1501c2c9e2fa70e
jespe <jesperkauppinen@gmail.com>
2021-12-02 12:37:40 +0200

    single page

--- views/gallery.py
+++ views/gallery.py
@@ -27,3 +27,23 @@ def index():
         }
         all_artwork_details.append(artwork_details)
     return render_template('gallery/index.html', user=current_user, artworks=all_artwork_details)
+
+
+@gallery.get('/<int:id_>')
+def single(id_):
+    artwork_obj = Artwork.query.filter_by(id=id_).first()
+    if Likes.query.filter_by(artwork_id=id_, user_id=artwork_obj.owner_id).first():
+        like_status = True
+    else:
+        like_status = False
+    artwork_details = {
+        "artwork_id": id_,
+        "href": artwork_obj.filename,
+        "date": artwork_obj.date,
+        "title": artwork_obj.title,
+        "owner_id": artwork_obj.owner_id,
+        "owner_name": User.query.filter_by(id=artwork_obj.owner_id).first().username,
+        "like_count": Likes.query.filter_by(artwork_id=artwork_obj.owner_id).count(),
+        "like_status": like_status,
+    }
+    return render_template('gallery/single.html', user=current_user, artworks=artwork_details)
